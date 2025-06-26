1e5792e273c98210b46f13cf5160cca167abd6cc
Mateus Manhani <mateus.manhani.dias@gmail.com>
2025-06-26 23:06:37 +0100

    Created Gui-client

---
+++ gui-client/src/main/java/org/healthapp/GuiClientApp.java
@@ -0,0 +1,11 @@
+package org.healthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiClientApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GuiClientApp.class, args);
+    }
+}
