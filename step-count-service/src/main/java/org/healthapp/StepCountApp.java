03ee019d5e45d19052a4fa2ae6b25a0d19d835de
Mateus Manhani <mateus.manhani.dias@gmail.com>
2025-07-01 20:51:52 +0100

    Created StepCountService and added as a chilld project

---
+++ step-count-service/src/main/java/org/healthapp/StepCountApp.java
@@ -0,0 +1,28 @@
+package org.healthapp;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class StepCountApp {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        ConfigurableApplicationContext context = SpringApplication.run(StepCountApp.class, args);
+
+//        HeartRateService heartRateService = context.getBean(HeartRateService.class);
+//
+//        Server server = ServerBuilder
+//                .forPort(50051)
+//                .addService(new HeartRateGrpcController(heartRateService))
+//                .build()
+//                .start();
+//
+//        System.out.println("✅ Heart Rate gRPC server started on port 50051");
+//        server.awaitTermination();
+    }
+}
